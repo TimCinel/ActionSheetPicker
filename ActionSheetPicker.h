9058c936eba664946d3ae280e617888c312e26ca
Tim Cinel <email@timcinel.com>
2011-02-01 21:45:01 +1100

    Added DatePicker functionality. Not yet thoroughly tested

--- ActionSheetPicker.h
+++ ActionSheetPicker.h
@@ -1,14 +1,13 @@
 //
 //  ActionSheetPicker.h
-//	https://github.com/sickanimations/ActionSheetPicker
-//  
-//  Improvements more than welcome.
+//  Spent
 //
-//  Created by Tim Cinel
-//  http://www.timcinel.com/
+//  Created by Tim Cinel on 3/01/11.
+//  Copyright 2011 Thunderous Playground. All rights reserved.
 //
-//	Easily present an ActionSheet with a PickerView, allowing user to select from a number of immutible options.
-//	Based on the HTML drop-down alternative found in mobilesafari.
+//
+//	Easily present an ActionSheet with a PickerView to select from a number of immutible options,
+//	based on the drop-down replacement in mobilesafari.
 //
 //	Some code derived from marcio's post on Stack Overflow [ http://stackoverflow.com/questions/1262574/add-uipickerview-a-button-in-action-sheet-how ]  
 
@@ -17,37 +16,61 @@
 
 @interface ActionSheetPicker : NSObject <UIPickerViewDelegate, UIPickerViewDataSource> {
 	UIView *_view;
+	
 	NSArray *_data;
 	NSInteger _selectedIndex;
+	
+	UIDatePickerMode _datePickerMode;
+	NSDate *_selectedDate;
+	
 	id _target;
 	SEL _action;
 	
 	UIActionSheet *_actionSheet;
 	UIPickerView *_pickerView;
+	UIDatePicker *_datePickerView;
 	NSInteger _pickerPosition;
+	
 	BOOL _convenientObject;
 
 }
 
 @property (nonatomic, retain) UIView *view;
+
 @property (nonatomic, retain) NSArray *data;
 @property (nonatomic, assign) NSInteger selectedIndex;
+
+@property (nonatomic, assign) UIDatePickerMode datePickerMode;
+@property (nonatomic, retain) NSDate *selectedDate;
+
 @property (nonatomic, retain) id target;
 @property (nonatomic, assign) SEL action;
 
 @property (nonatomic, retain) UIActionSheet *actionSheet;
 @property (nonatomic, retain) UIPickerView *pickerView;
+@property (nonatomic, retain) UIDatePicker *datePickerView;
 @property (nonatomic, assign) NSInteger pickerPosition;
 @property (nonatomic, assign) BOOL convenientObject;
 
-//display action picker in View loaded with data, with item selectedIndex selected. On dismissal, [target action:(NSNumber *)selectedIndex] is called
-//no memory management required
+//no memory management required for convenience methods
+
+//display actionsheet picker inside View, loaded with strings from data, with item selectedIndex selected. On dismissal, [target action:(NSNumber *)selectedIndex] is called
 + (void)displayActionPickerWithView:(UIView *)aView data:(NSArray *)data selectedIndex:(NSInteger)selectedIndex target:(id)target action:(SEL)action;
 
-- (id)initWithContainingView:(UIView *)aView data:(NSArray *)someData selectedIndex:(NSInteger)index target:(id)target action:(SEL)action;
+//display actionsheet datepicker in datePickerMode inside View with selectedDate selected. On dismissal, [target action:(NSDate *)selectedDate] is called
++ (void)displayActionPickerWithView:(UIView *)aView datePickerMode:(UIDatePickerMode)datePickerMode selectedDate:(NSDate *)selectedDate target:(id)target action:(SEL)action;
 
+- (id)initWithContainingView:(UIView *)aView target:(id)target action:(SEL)action;
+- (id)initForDataWithContainingView:(UIView *)aView data:(NSArray *)someData selectedIndex:(NSInteger)index target:(id)target action:(SEL)action;
+- (id)initForDateWithContainingView:(UIView *)aView datePickerMode:(UIDatePickerMode)datePickerMode selectedDate:(NSDate *)selectedDate target:(id)target action:(SEL)action;
+
+//implementation
 - (void)showActionPicker;
+- (void)showDataPicker;
+- (void)showDatePicker;
 
 - (void)actionPickerDone;
+
+- (void)eventForDatePicker:(id)sender;
 
 @end
