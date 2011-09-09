3a026d04110a90d743ddb9640ff829474fa7588d
Peter Goldsmith <peter.goldsmith@net-effects.com.au>
2011-09-09 10:03:00 +1000

    Added the WTFPL

--- ActionSheetPicker.h
+++ ActionSheetPicker.h
@@ -10,6 +10,12 @@
 //	based on the drop-down replacement in mobilesafari.
 //
 //	Some code derived from marcio's post on Stack Overflow [ http://stackoverflow.com/questions/1262574/add-uipickerview-a-button-in-action-sheet-how ]  
+//
+//  This program is free software. It comes without any warranty, to
+//  the extent permitted by applicable law. You can redistribute it
+//  and/or modify it under the terms of the Do What The Fuck You Want
+//  To Public License, Version 2, as published by Sam Hocevar. See
+//  http://sam.zoy.org/wtfpl/COPYING for more details. 
 
 #import <Foundation/Foundation.h>
 
