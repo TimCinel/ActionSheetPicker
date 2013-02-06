689c4bbdd7b3f837cef134860b1d58b8c8b359a0
Boris Bügling <b.buegling@gingco.net>
2013-02-06 10:09:24 +0100

    Needs UIKit, so it should import the header.

--- Pickers/AbstractActionSheetPicker.h
+++ Pickers/AbstractActionSheetPicker.h
@@ -25,7 +25,7 @@
 //SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 
 @interface AbstractActionSheetPicker : NSObject
