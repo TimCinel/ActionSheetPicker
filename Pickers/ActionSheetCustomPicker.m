f5abe57b601731da6432afaed465321169ff0304
Nikos Mouzakitis <spereneka@gmail.com>
2014-06-04 19:33:45 +0300

    Modify delegate for more customization
    Modified delegate function configurePickerView: to pass a
    reference to the ActionSheetPicker object as well apart from the
    pickerView.

--- Pickers/ActionSheetCustomPicker.m
+++ Pickers/ActionSheetCustomPicker.m
@@ -52,8 +52,8 @@
     pv.showsSelectionIndicator = YES;
     
     // Allow the delegate to override and set additional configs
-    if ([_delegate respondsToSelector:@selector(configurePickerView:)]) {
-        [_delegate configurePickerView:pv];
+    if ([_delegate respondsToSelector:@selector(actionSheetPicker:configurePickerView:)]) {
+        [_delegate actionSheetPicker:self configurePickerView:pv];
     }
     self.pickerView = pv;
     return pv;
