e559a3269792e69bd87bb5c96da2cc10a7934036
Petr Korolev <sky4winder@gmail.com>
2014-08-12 17:52:26 +0300

    add bump script

---
+++ bump.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+readme = File.read("README.md")
+
+re = /(\d+\.\d+\.)(\d+)/m
+
+result = re.match(readme)
+puts result
+unless result
+	puts "Not found"
+	exit
+end
+
+pref = result[1]
+new_version = result[2].to_i + 1
+
+bumped_vers = pref + new_version.to_s
+puts bumped_vers
+
+`sed -i "" "s/#{result}/#{bumped_vers}/" README.md`
+`sed -i "" "s/#{result}/#{bumped_vers}/" ActionSheetPicker-3.0.podspec`
