24e8a17f6f82b1f42213c232615d23c1d255f1cb
Petr Korolev <sky4winder@gmail.com>
2014-08-12 18:08:13 +0300

    update script

--- bump.rb
+++ bump.rb
@@ -5,7 +5,6 @@ readme = File.read("README.md")
 re = /(\d+\.\d+\.)(\d+)/m
 
 result = re.match(readme)
-puts result
 unless result
 	puts "Not found"
 	exit
@@ -15,11 +14,20 @@ pref = result[1]
 new_version = result[2].to_i + 1
 
 bumped_vers = pref + new_version.to_s
-puts bumped_vers
+puts "Bump version: #{result} -> #{bumped_vers}"
 
-`sed -i "" "s/#{result}/#{bumped_vers}/" README.md`
-`sed -i "" "s/#{result}/#{bumped_vers}/" ActionSheetPicker-3.0.podspec`
-`git commit --all -m "Update podspec"`
-`git tag #{bumped_vers}`
-`git push`
-`pod trunk push ./ActionSheetPicker-3.0.podspec`
+def execute_line(line)
+	value =%x[#{line}]
+	puts value
+	if $?.exitstatus != 0
+		puts "Error -> terminate"
+		exit
+	end
+end
+
+execute_line("sed -i \"\" \"s/#{result}/#{bumped_vers}/\" README.md")
+execute_line("sed -i \"\" \"s/#{result}/#{bumped_vers}/\" ActionSheetPicker-3.0.podspec")
+execute_line("git commit --all -m \"Update podspec\"")
+execute_line("git tag #{bumped_vers}")
+execute_line("git push")
+execute_line("git push --tags")
