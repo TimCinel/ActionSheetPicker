739ad687b5ca1c69b17647d37e235536c3f0c311
Petr Korolev <sky4winder@gmail.com>
2014-09-30 11:12:09 +0300

    add freamework and dependices

---
+++ CoreActionSheetPicker/CoreActionSheetPicker/CoreActionSheetPicker/CoreActionSheetPicker.h
@@ -0,0 +1,19 @@
+//
+//  CoreActionSheetPicker.h
+//  CoreActionSheetPicker
+//
+//  Created by Petr Korolev on 30/09/14.
+//  Copyright (c) 2014 Petr Korolev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreActionSheetPicker.
+FOUNDATION_EXPORT double CoreActionSheetPickerVersionNumber;
+
+//! Project version string for CoreActionSheetPicker.
+FOUNDATION_EXPORT const unsigned char CoreActionSheetPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreActionSheetPicker/PublicHeader.h>
+
+
