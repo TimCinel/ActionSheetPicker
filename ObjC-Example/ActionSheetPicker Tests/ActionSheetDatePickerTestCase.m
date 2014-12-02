11ca30784c4b5f89a4462f5bbbbf238129837d49
Petr Korolev <sky4winder@gmail.com>
2014-12-02 11:59:25 +0200

    add default init tests

--- ObjC-Example/ActionSheetPicker Tests/ActionSheetDatePickerTestCase.m
+++ ObjC-Example/ActionSheetPicker Tests/ActionSheetDatePickerTestCase.m
@@ -11,7 +11,7 @@
 #import "AbstractActionSheetPicker+CustomButton.h"
 
 static const int countdownTestInt = 360;
-UIView *origin;
+UIView           *origin;
 
 @interface ActionSheetDatePickerTestCase : XCTestCase
 @property(nonatomic, strong) ActionSheetDatePicker *actionSheetDatePicker;
@@ -22,35 +22,62 @@ UIView *origin;
     NSString *_title;
 }
 
-+(void)setUp{
-    UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-    UIView *topView = window.rootViewController.view;
-    origin = topView;    
++ (void)setUp
+{
+    UIWindow *window  = [[UIApplication sharedApplication] keyWindow];
+    UIView   *topView = window.rootViewController.view;
+    origin = topView;
 }
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
-    _title = @"Title";
+    _title                 = @"Title";
     _actionSheetDatePicker = [[ActionSheetDatePicker alloc] initWithTitle:@"Test title" datePickerMode:UIDatePickerModeDate selectedDate:[NSDate date] target:nil action:nil origin:origin cancelAction:nil];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown {
+- (void)tearDown
+{
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+}
+
+- (void)testInitPicker
+{
+    _actionSheetDatePicker = [[ActionSheetDatePicker alloc] initWithTarget:self successAction:@selector(exampleSelector) cancelAction:@selector(exampleSelector) origin:origin];
+    XCTAssertNotNil(_actionSheetDatePicker);
+}
+
+- (void)testInitPicker2
+{
+    _actionSheetDatePicker = [[ActionSheetDatePicker alloc] initWithTitle:@"" datePickerMode:UIDatePickerModeCountDownTimer selectedDate:[NSDate date] doneBlock:nil cancelBlock:nil origin:origin];
+    XCTAssertNotNil(_actionSheetDatePicker);
+}
+
+- (void)testInitPicker3
+{
+    _actionSheetDatePicker = [[ActionSheetDatePicker alloc] initWithTitle:@"" datePickerMode:UIDatePickerModeCountDownTimer selectedDate:[NSDate date] target:self action:@selector(exampleSelector) origin:origin];
+    XCTAssertNotNil(_actionSheetDatePicker);
+}
+
+- (void)testInitPicker4
+{
+    _actionSheetDatePicker = [[ActionSheetDatePicker alloc] initWithTitle:@"" datePickerMode:UIDatePickerModeCountDownTimer selectedDate:NSDate.date target:self action:@selector(exampleSelector) origin:origin cancelAction:@selector(exampleSelector)];
+    XCTAssertNotNil(_actionSheetDatePicker);
 }
 
 - (void)testPickerWithCustomActionBlockOnButton
 {
     NSString *custom_title = @"Custom label:";
-    
+
     [_actionSheetDatePicker addCustomButtonWithTitle:custom_title actionBlock:^{
         NSLog(@"Test block invoked");
     }];
     [_actionSheetDatePicker showActionSheetPicker];
-    
+
     [_actionSheetDatePicker pressFirstCustomButton];
-    
+
     XCTAssertNotNil(_actionSheetDatePicker);
 }
 
@@ -60,7 +87,7 @@ UIView *origin;
         NSLog(@"Test block invoked");
     }];
     [_actionSheetDatePicker showActionSheetPicker];
-    
+
     [_actionSheetDatePicker pressFirstCustomButton];
 
     XCTAssertNotNil(_actionSheetDatePicker);
@@ -70,9 +97,9 @@ UIView *origin;
 {
     [_actionSheetDatePicker addCustomButtonWithTitle:_title actionBlock:nil];
     [_actionSheetDatePicker showActionSheetPicker];
-    
+
     [_actionSheetDatePicker pressFirstCustomButton];
-    
+
     XCTAssertNotNil(_actionSheetDatePicker);
 }
 
@@ -80,9 +107,9 @@ UIView *origin;
 {
     [_actionSheetDatePicker addCustomButtonWithTitle:nil actionBlock:nil];
     [_actionSheetDatePicker showActionSheetPicker];
-    
+
     [_actionSheetDatePicker pressFirstCustomButton];
-    
+
     XCTAssertNotNil(_actionSheetDatePicker);
 }
 
@@ -100,9 +127,9 @@ UIView *origin;
 {
     [_actionSheetDatePicker addCustomButtonWithTitle:nil target:self selector:@selector(exampleSelector)];
     [_actionSheetDatePicker showActionSheetPicker];
-    
+
     [_actionSheetDatePicker pressFirstCustomButton];
-    
+
     XCTAssertNotNil(_actionSheetDatePicker);
 }
 
@@ -110,9 +137,9 @@ UIView *origin;
 {
     [_actionSheetDatePicker addCustomButtonWithTitle:_title target:self selector:nil];
     [_actionSheetDatePicker showActionSheetPicker];
-    
+
     [_actionSheetDatePicker pressFirstCustomButton];
-    
+
     XCTAssertNotNil(_actionSheetDatePicker);
 }
 
@@ -120,9 +147,9 @@ UIView *origin;
 {
     [_actionSheetDatePicker addCustomButtonWithTitle:_title target:nil selector:@selector(exampleSelector)];
     [_actionSheetDatePicker showActionSheetPicker];
-    
+
     [_actionSheetDatePicker pressFirstCustomButton];
-    
+
     XCTAssertNotNil(_actionSheetDatePicker);
 }
 
@@ -130,9 +157,9 @@ UIView *origin;
 {
     [_actionSheetDatePicker addCustomButtonWithTitle:nil target:nil selector:nil];
     [_actionSheetDatePicker showActionSheetPicker];
-    
+
     [_actionSheetDatePicker pressFirstCustomButton];
-    
+
     XCTAssertNotNil(_actionSheetDatePicker);
 }
 
@@ -150,11 +177,11 @@ UIView *origin;
 {
     _actionSheetDatePicker = [[ActionSheetDatePicker alloc] initWithTitle:@"Test" datePickerMode:UIDatePickerModeCountDownTimer selectedDate:nil doneBlock:^(ActionSheetDatePicker *picker, id selectedDate, id origin) {
         XCTAssertEqualObjects(selectedDate, @(countdownTestInt));
-    } cancelBlock:nil origin:origin];
+    }                                                         cancelBlock:nil origin:origin];
 
     _actionSheetDatePicker.countDownDuration = countdownTestInt;
     [_actionSheetDatePicker showActionSheetPicker];
-    UIDatePicker *picker = (UIDatePicker *)_actionSheetDatePicker.pickerView;
+    UIDatePicker *picker = (UIDatePicker *) _actionSheetDatePicker.pickerView;
     [_actionSheetDatePicker pressDoneButton];
 
     XCTAssertNotNil(_actionSheetDatePicker);
