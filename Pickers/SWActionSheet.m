56cc295c8fef742ce281c4148ad6d69180dc228c
Derek Trauger <dtrauger@derektrauger.com>
2015-03-05 09:18:45 -0500

    Added support for upside down portrait;

--- Pickers/SWActionSheet.m
+++ Pickers/SWActionSheet.m
@@ -206,4 +206,20 @@ static const enum UIViewAnimationOptions options = UIViewAnimationOptionCurveEas
 	return [UIApplication sharedApplication].statusBarHidden;
 }
 
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return NO;
+}
+
+// iOS6 support
+// ---
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+-(NSUInteger)supportedInterfaceOrientations{
+    return UIInterfaceOrientationMaskAll;
+}
+
 @end
