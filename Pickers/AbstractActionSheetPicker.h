c0064f36f55aa7d0bd87baa979103c61daacf08f
Dmitry Goykolov <goykolov@gmail.com>
2015-04-01 21:27:19 +0200

    Added "tag" to AbstractActionSheet

--- Pickers/AbstractActionSheetPicker.h
+++ Pickers/AbstractActionSheetPicker.h
@@ -61,6 +61,7 @@ static NSString *const kActionType    = @"buttonAction";
 static NSString *const kActionTarget  = @"buttonActionTarget";
 
 @interface AbstractActionSheetPicker : NSObject<UIPopoverControllerDelegate>
+@property (nonatomic, assign) NSInteger tag;
 @property (nonatomic, strong) UIToolbar* toolbar;
 @property (nonatomic, copy) NSString *title;
 @property (nonatomic, strong) UIView *pickerView;
