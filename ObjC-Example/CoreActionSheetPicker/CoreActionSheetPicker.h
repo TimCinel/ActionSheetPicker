893d976ca849759ec23923e416a18305c6305d49
Petr Korolev <sky4winder@gmail.com>
2015-04-14 16:47:24 +0300

    add base framework

---
+++ ObjC-Example/CoreActionSheetPicker/CoreActionSheetPicker.h
@@ -0,0 +1,19 @@
+//
+//  CoreActionSheetPicker.h
+//  CoreActionSheetPicker
+//
+//  Created by Petr Korolev on 14/04/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreActionSheetPicker.
+FOUNDATION_EXPORT double CoreActionSheetPickerVersionNumber;
+
+//! Project version string for CoreActionSheetPicker.
+FOUNDATION_EXPORT const unsigned char CoreActionSheetPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreActionSheetPicker/PublicHeader.h>
+
+
