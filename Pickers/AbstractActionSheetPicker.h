5ce5ea7dd0d5af7c7da0fae4ea95bd4b3937567d
Tommy Goode <tommy@tgoode.com>
2016-05-28 16:48:37 -0500

    Adding the ability to disable popover behavior on iPad per picker instance.

--- Pickers/AbstractActionSheetPicker.h
+++ Pickers/AbstractActionSheetPicker.h
@@ -81,6 +81,7 @@ static NSString *const kActionTarget  = @"buttonActionTarget";
 @property (nonatomic, retain) Class popoverBackgroundViewClass; //allow popover customization on iPad
 @property (nonatomic) UIInterfaceOrientationMask supportedInterfaceOrientations; // You can set your own supportedInterfaceOrientations value to prevent dismissing picker in some special cases.
 @property (nonatomic) TapAction tapDismissAction; // Specify, which action should be fired in case of tapping outside of the picker (on top darkened side). Default is TapActionNone.
+@property (nonatomic) BOOL popoverDisabled; // Disable popover behavior on iPad
 
 // For subclasses.
 - (instancetype)initWithTarget:(id)target successAction:(SEL)successAction cancelAction:(SEL)cancelActionOrNil origin:(id)origin;
