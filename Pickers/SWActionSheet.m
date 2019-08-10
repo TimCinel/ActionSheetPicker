cd2d9f55a098c1b6bf0510c3ba1a5d9229d2ccb3
Arnold Tang <arnoldtang@gmail.com>
2019-08-11 00:58:16 +0800

    Support iOS 13 Dark Mode

--- Pickers/SWActionSheet.m
+++ Pickers/SWActionSheet.m
@@ -126,7 +126,12 @@ static const enum UIViewAnimationOptions options = UIViewAnimationOptionCurveEas
         _windowLevel = windowLevel;
         self.backgroundColor = [UIColor colorWithWhite:0.f alpha:0.0f];
         _bgView = [UIView new];
-        _bgView.backgroundColor = [UIColor colorWithRed:247.f/255.f green:247.f/255.f blue:247.f/255.f alpha:1.0f];
+        if (@available(iOS 13.0, *)) { // Support iOS 13 system dark mode
+            _bgView.backgroundColor = [UIColor systemBackgroundColor];
+        }
+        else {
+            _bgView.backgroundColor = [UIColor colorWithRed:247.f/255.f green:247.f/255.f blue:247.f/255.f alpha:1.0f];
+        }
         [self addSubview:_bgView];
         [self addSubview:view];
     }
