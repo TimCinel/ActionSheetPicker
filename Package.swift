37202cee808a32c3638b1b9671d8f107f7df5eb0
Petr Korolev <sky4winder@gmail.com>
2020-02-16 01:49:21 +0700

    added swift package

--- Package.swift
+++ Package.swift
@@ -4,25 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "ActionSheetPicker",
+    name: "ActionSheetPicker-3.0",
+                      targets: [.target(name: "Alamofire",
+                                        path: "Source")],
+
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "ActionSheetPicker",
-            targets: ["ActionSheetPicker"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            name: "ActionSheetPicker-3.0",
+            targets: ["ActionSheetPicker-3.0"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ActionSheetPicker",
-            dependencies: []),
-        .testTarget(
-            name: "ActionSheetPickerTests",
-            dependencies: ["ActionSheetPicker"]),
-    ]
+            name: "CoreActionSheetPicker",
+            path: "CoreActionSheetPicker")
+        ],
+    swiftLanguageVersions: [.v5]
 )
