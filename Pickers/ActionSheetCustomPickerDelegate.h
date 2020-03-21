78bda37e5b8595a79bd9a441c208d92b50458638
Manuel Zierenner <ddr8@web.de>
2020-03-21 14:12:34 +0100

    files changed

--- Pickers/ActionSheetCustomPickerDelegate.h
+++ Pickers/ActionSheetCustomPickerDelegate.h
@@ -6,25 +6,26 @@
 //  Copyright (c) 2012 Club 15CC. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import "AbstractActionSheetPicker.h"
+#import <UIKit/UIKit.h>
+
 
 @protocol ActionSheetCustomPickerDelegate <UIPickerViewDelegate, UIPickerViewDataSource>
 
 @optional
 
-/** 
+/**
  Allow the delegate to override default settings for the picker
- 
+
  Allows for instance, ability to set separate delegates and data sources as well as GUI settings on the UIPickerView
  If not defined and explicily overridden then this class will be the delegate and dataSource.
  */
 - (void)configurePickerView:(UIPickerView *)pickerView DEPRECATED_ATTRIBUTE;
 - (void)actionSheetPicker:(AbstractActionSheetPicker *)actionSheetPicker configurePickerView:(UIPickerView *)pickerView;
 
-/** 
- Success callback 
- 
+/**
+ Success callback
+
  \param actionSheetPicker   .pickerView property accesses the picker.  Requires a cast to UIView subclass for the picker
  \param origin              The entity which launched the ActionSheetPicker
  */
