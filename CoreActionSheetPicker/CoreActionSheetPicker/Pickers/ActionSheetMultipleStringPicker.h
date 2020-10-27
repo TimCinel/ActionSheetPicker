fa3d575052c760702b5f90e69cff5397cacf6c40
ad-sho-loko <ad.sho.loko@gmail.com>
2020-10-27 15:58:04 +0900

    fix typo in the comment

--- CoreActionSheetPicker/CoreActionSheetPicker/Pickers/ActionSheetMultipleStringPicker.h
+++ CoreActionSheetPicker/CoreActionSheetPicker/Pickers/ActionSheetMultipleStringPicker.h
@@ -51,7 +51,7 @@ typedef void(^ActionMultipleStringCancelBlock)(ActionSheetMultipleStringPicker *
  */
 + (instancetype)showPickerWithTitle:(NSString *)title rows:(NSArray *)data initialSelection:(NSArray *)indexes target:(id)target successAction:(SEL)successAction cancelAction:(SEL)cancelActionOrNil origin:(id)origin;
 
-/// Create and display an action sheet picker with assigned target actions on sucess/cancel, but don't display until a subsequent call to "showActionPicker".
+/// Create and display an action sheet picker with assigned target actions on success/cancel, but don't display until a subsequent call to "showActionPicker".
 - (instancetype)initWithTitle:(NSString *)title rows:(NSArray *)data initialSelection:(NSArray *)indexes target:(id)target successAction:(SEL)successAction cancelAction:(SEL)cancelActionOrNil origin:(id)origin;
 
 
